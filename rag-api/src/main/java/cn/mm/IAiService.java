6ff6857b6dfe70abf30f14c86ba168e2112325f2
AAAmy <10165760+aaamyyy@user.noreply.gitee.com>
2025-10-03 17:00:24 +0800

    feat: 上传向量数据库、测试使用

---
+++ rag-api/src/main/java/cn/mm/IAiService.java
@@ -0,0 +1,12 @@
+package cn.mm;
+
+import org.springframework.ai.chat.ChatResponse;
+import reactor.core.publisher.Flux;
+
+public interface IAiService {
+
+    ChatResponse generate(String model, String message);
+
+    Flux<ChatResponse> generateStream(String model, String message);
+
+}
